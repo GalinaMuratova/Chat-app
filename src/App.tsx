02b2e4a36c55b55b4f540446dbdc74a02828252d
GalinaMuratova <ufkz200147@gmail.com>
2023-06-19 15:33:47 +0600

    Сделала отрисовку данных

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,87 @@ import './App.css'
 const App = () => {
     const [author, setAuthor] = useState('');
     const [message, setMessage] = useState('');
-    const [allInfo, setAllInfo] = useState<{ author: string; message: string; datetime: string; }[]>([]);
     const [dateTime, setDateTime] = useState('');
+    const [allInfo, setAllInfo] = useState<{ author: string; message: string; datetime: string; }[]>([]);
+
+
+    const sendMessage = (e:React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('sent');
+    };
+
+    const getMessage = async () => {
+        const url = `http://146.185.154.90:8000/messages?datetime=${dateTime}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            if (data.length > 0) {
+                setAllInfo((prevState) => [...prevState, ...data]);
+                setDateTime(data[data.length - 1].datetime);
+                console.log(data);
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    };
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            console.log("hi");
-        }, 3000);
+        const interval = setInterval(getMessage, 3000);
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
-        console.log('fetch');
+        getMessage();
     }, []);
 
 
     return (
-      <div className="container">
-          <div className="container">
-              <h1>Chat App</h1>
-              <div className="content" id="content"></div>
-              <div className="row">
-                  <div className="col-md-12">
-                      <form id="send">
-                          <div className="form-group">
-                              <label htmlFor="author">Author:</label>
-                              <input type="text" className="form-control" id="author"/>
-                          </div>
-                          <div className="form-group">
-                              <label htmlFor="message">Message:</label>
-                              <textarea className="form-control" id="message"></textarea>
-                          </div>
-                          <button type="submit" className="btn btn-primary">Send</button>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
+        <div className="container">
+            <h1 className="main-title">Chat App</h1>
+            <div className="content">
+                {allInfo.map((msg, index) => (
+                    <div key={index} className="block">
+                        <div className="card-header">
+                            Author: {msg.author}
+                            Data: {msg.datetime}
+                        </div>
+                        <div className="card-body">
+                            <blockquote className="blockquote mb-0">
+                                <p>{msg.message}</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <form onSubmit={sendMessage}>
+                        <div className="form-group">
+                            <label htmlFor="author">Author:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="author"
+                                value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message">Message:</label>
+                            <textarea
+                                className="form-control"
+                                id="message"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                            ></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Send
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
   );
 };
 
