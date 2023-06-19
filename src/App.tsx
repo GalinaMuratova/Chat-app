ea92eadc02a016ae91536f7cb3559316ef141376
GalinaMuratova <ufkz200147@gmail.com>
2023-06-19 14:15:32 +0600

    Добавила изначальную разметку и стили

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,25 @@ const App = () => {
 
   return (
       <div className="container">
-
+          <div className="container">
+              <h1>Chat App</h1>
+              <div className="content" id="content"></div>
+              <div className="row">
+                  <div className="col-md-12">
+                      <form id="send">
+                          <div className="form-group">
+                              <label htmlFor="author">Author:</label>
+                              <input type="text" className="form-control" id="author"/>
+                          </div>
+                          <div className="form-group">
+                              <label htmlFor="message">Message:</label>
+                              <textarea className="form-control" id="message"></textarea>
+                          </div>
+                          <button type="submit" className="btn btn-primary">Send</button>
+                      </form>
+                  </div>
+              </div>
+          </div>
       </div>
   );
 };
